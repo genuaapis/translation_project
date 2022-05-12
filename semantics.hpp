3ebf39b53c14e8a3862cbf8ff3bc4597adb4e9b1
Salvatore <58443504+genuaapis@users.noreply.github.com>
2022-05-11 21:15:57 -0500

    Update semantics.hpp

--- semantics.hpp
+++ semantics.hpp
@@ -3,12 +3,15 @@
 
 #include <vector>
 #include <string>
+#include <iostream>
 #include "token.hpp"
 #include "parser.hpp"
 
-void traversal(parse_node*, std::vector<std::string>&, std::vector<int>&, bool&);
-void push(std::string, std::vector<std::string>&);
-void pop(std::vector<std::string>&);
-int find(std::string, std::vector<std::string>&);
+void traversal(std::ostream*, parse_node*, std::vector<std::string>&, std::vector<int>&, bool&);
+void push(std::ostream*, std::string, std::vector<std::string>&);
+void pop(std::ostream*, std::vector<std::string>&);
+int find(std::ostream*, std::string, std::vector<std::string>&);
+std::string new_var();
+std::string new_label();
 
 #endif
